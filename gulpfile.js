0a736f490bfb8db50fd5f6835bb77d020b52dfe1
tyson_taylor <tyson.james.taylor@gmail.com>
2016-04-15 16:21:11 -0600

    Fix gulpfile.js, >>add gulp-util, browser-sync, cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,58 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-//var uglify = require('gulp-uglify')
-var uglifycss = require('gulp-uglifycss');
-var ngAnnotate = require('gulp-ng-annotate');
-var server = require('gulp-webserver');
-var htmlmin = require('gulp-htmlmin');
-var babel = require('gulp-babel');
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var concat = require( 'gulp-concat' );
+var uglify = require( 'gulp-uglify' )
+var uglifycss = require( 'gulp-uglifycss' );
+var ngAnnotate = require( 'gulp-ng-annotate' );
+// var server = require('gulp-webserver');
+var htmlmin = require( 'gulp-htmlmin' );
+var babel = require( 'gulp-babel' );
 
 
-var watcher = gulp.watch(['./main/js/**/*.js', './main/styles/*.scss', './main/styles/*.css', './main/views/**/**/*.html', './main/*.html'], ['default']);
-watcher.on('change', function( event ) {
-        console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
-});
+var watcher = gulp.watch( [ './main/js/**/*.js', './main/styles/*.scss', './main/styles/*.css', './main/views/**/**/*.html', './main/*.html' ], [ 'default' ] );
+watcher.on( 'change', function ( event ) {
+  console.log( 'File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...' );
+} );
 
-gulp.task('sass', function() {
-    gulp.src('./main/styles/*.scss')
-        .pipe(sass())
-        .pipe(uglifycss())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('./public/styles'))
-});
-gulp.task('css', function() {
-  gulp.src('./main/styles/*.css')
-  .pipe(uglifycss())
-  .pipe(gulp.dest('./public/styles'))
-});
-gulp.task('javascript', function() {
-    gulp.src('./main/js/**/*.js')
-        //.pipe(ngAnnotate())
-        // .pipe(uglify())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('./public/scripts'))
-});
-gulp.task('html', function() {
-    gulp.src('./main/views/**/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./public/views'))
-});
-gulp.task('index', function() {
-    gulp.src('./main/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('./public'))
-});
-gulp.task('default', ['sass', 'css', 'javascript', 'html', 'index']);
+gulp.task( 'sass', function () {
+  gulp.src( './main/styles/*.scss' )
+    .pipe( sass() )
+    .pipe( uglifycss() )
+    .pipe( concat( 'styles.css' ) )
+    .pipe( gulp.dest( './public/styles' ) )
+} );
+
+gulp.task( 'css', function () {
+  gulp.src( './main/styles/*.css' )
+    .pipe( uglifycss() )
+    .pipe( gulp.dest( './public/styles' ) )
+} );
+
+gulp.task( 'javascript', function () {
+  gulp.src( './main/js/**/*.js' )
+    .pipe( ngAnnotate() )
+    .pipe( babel( {
+      presets: [ 'es2015' ]
+    } ) )
+    .pipe( uglify() )
+    .pipe( concat( 'all.min.js' ) )
+    .pipe( gulp.dest( './public/scripts' ) )
+} );
+
+gulp.task( 'html', function () {
+  gulp.src( './main/views/**/*.html' )
+    .pipe( htmlmin( {
+      collapseWhitespace: true
+    } ) )
+    .pipe( gulp.dest( './public/views' ) )
+} );
+
+gulp.task( 'index', function () {
+  gulp.src( './main/*.html' )
+    .pipe( htmlmin( {
+      collapseWhitespace: true
+    } ) )
+    .pipe( gulp.dest( './public' ) )
+} );
+
+gulp.task( 'default', [ 'sass', 'css', 'javascript', 'html', 'index' ] );
